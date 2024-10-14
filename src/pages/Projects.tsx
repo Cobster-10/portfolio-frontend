cae946a20dafd848aef02412a941ee0e7c1e9eb6
Cobster-10 <coby.kuruvilla@gmail.com>
2024-10-13 20:14:09 -0500

    loading optimization

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,51 +1,70 @@
+import { VariableSizeList as List } from "react-window";
+
 import { Box, Flex, HStack, Stack } from "@chakra-ui/react";
 import { Canvas } from "@react-three/fiber";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { lazy, Suspense, useEffect, useState, useCallback } from "react";
 import { Vector3 } from "three";
 import CustomLoader from "../components/CustomLoader";
 import Robot from "../models/Robot";
 import ProjectImage from "../components/ProjectImage";
 import ProjectDescription from "../components/ProjectDescription";
-import Project from "../components/Project";
+// import Project from "../components/Project";
 import CameraAnimator from "../models/CameraAnimator";
 import { MoveCameraProvider, useMoveCamera } from "../Hooks/useMoveCamera";
 import axios from "axios";
+import { useInView } from "react-intersection-observer";
+
+const Project = lazy(() => import("../components/Project"));
 
 const Projects = () => {
   const [isRotating, setIsRotating] = useState(false);
-
+  
 
   const {
     state: moveCamera,
     setState: setMoveCamera,
     RobotAnimation,
     setRobotAnimation,
-    AnimationCase, 
-    setAnimationCase
-  } = useMoveCamera() as { state: any; setState: any; RobotAnimation: any; setRobotAnimation: any; AnimationCase: any; setAnimationCase: any; };
+    AnimationCase,
+    setAnimationCase,
+  } = useMoveCamera() as {
+    state: any;
+    setState: any;
+    RobotAnimation: any;
+    setRobotAnimation: any;
+    AnimationCase: any;
+    setAnimationCase: any;
+  };
   console.log(moveCamera);
 
   const [projects, setProjects] = useState([]);
+  const [heights, setHeights] = useState<number[]>([]);
   useEffect(() => {
     console.log("hello");
     const fetchProjects = async () => {
-      axios.get("https://young-beyond-68677-346fa410ea44.herokuapp.com/api/db/projects")
-      .then((response) => {
-        setProjects(response.data);
-        for (let i = 0; i < response.data.length; i++) {
-          console.log(response.data[i]);
-        }
-        
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    }
+      axios
+        .get(
+          "https://young-beyond-68677-346fa410ea44.herokuapp.com/api/db/projects"
+        )
+        .then((response) => {
+          setProjects(response.data);
+          
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
     fetchProjects();
   }, []);
 
-
-
+  const setProjectHeight = useCallback((index: number, height: number) => {
+    setHeights((prevHeights) => {
+      const newHeights = [...prevHeights];
+      newHeights[index] = height;
+      return newHeights;
+    });
+  }, []);
+  const getItemSize = (index: number) => heights[index];
 
   return (
     <>
@@ -55,7 +74,6 @@ const Projects = () => {
         left={"0"}
         width={"25vw"}
         height={"88vh"}
-        
         className={isRotating ? "cursor-grabbing" : "cursor-grab"}
       >
         <Canvas camera={{ position: [0, 0, 5], near: 0.1, far: 1000 }}>
@@ -81,14 +99,61 @@ const Projects = () => {
 
       <Stack marginLeft={"25vw"} spacing={10}>
         {projects.map((project) => (
-          <Project
-            data={project}
-          />
+          <ProjectLoader  data={project} />
         ))}
-        {/* <Project data={{}}/>
-        <Project data={{}}/> */}
+        
       </Stack>
+
+      
+
+      {/* <Stack marginLeft={"25vw"} spacing={10}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <List
+            height={800} // The height of the container (visible window)
+            itemCount={projects.length} // Total number of items
+            itemSize={getItemSize} // Height of each item (adjust as needed)
+            width="100%" // The width of the container
+          >
+            {({index, style,}: {
+              index: number;
+              style: React.CSSProperties;
+            }) => (
+              <div style={style}>
+                <Project data={projects[index]} />
+              </div>
+            )}
+          </List>
+        </Suspense>
+      </Stack> */}
     </>
+  );
+
+
+
+};
+
+interface ProjectData {
+  // Define the structure of your project data here
+  data:any;
+  
+}
+
+const ProjectLoader = ({ data }: { data: ProjectData }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Only load once when the component is in view
+    threshold: 0.1,    // Load when 10% of the component is visible
+  });
+
+  return (
+    <div ref={ref}>
+      {inView ? (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Project data={data} />
+        </Suspense>
+      ) : (
+        <div style={{ height: '200px' }}> {/* Placeholder height */} </div>
+      )}
+    </div>
   );
 };
 
